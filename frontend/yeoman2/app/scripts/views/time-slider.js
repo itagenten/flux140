814885003c5078d711471b5ca3cd0b1d5a7ced96
Florian Sesser <fs@it-agenten.com>
2013-05-15 16:47:44 +0200

    Refs #4794: Add mouse whell scrolling.

--- frontend/yeoman2/app/scripts/views/time-slider.js
+++ frontend/yeoman2/app/scripts/views/time-slider.js
@@ -27,6 +27,23 @@ define([
                 max: 10,
                 change: this._changeHandler
             });
+
+            $('#timeSlider').bind('mousewheel DOMMouseScroll', function (e) {
+                var delta = 0, element = $(this), value, result, oe;
+                oe = e.originalEvent; // for jQuery >=1.7
+                value = element.slider('value');
+
+                if (oe.wheelDelta) {
+                    delta = -oe.wheelDelta;
+                }
+                if (oe.detail) {
+                    delta = oe.detail * 40;
+                }
+
+                value -= delta / 120;
+                element.slider('value', value);
+                return false;
+            });
         }
     });
 

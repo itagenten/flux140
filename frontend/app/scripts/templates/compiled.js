a6b5d98a3054a8f4b6a502d19cf23ce8abb36104
Florian Sesser <fs@it-agenten.com>
2013-10-25 16:55:47 +0200

    Add 'it-agenten' startup screen

--- frontend/app/scripts/templates/compiled.js
+++ frontend/app/scripts/templates/compiled.js
@@ -2,7 +2,7 @@ define(function(){
 
   this["JST"] = this["JST"] || {};
 
-  this["JST"]["app/scripts/templates/application.ejs"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<p>Not found: ' +((__t = ( window.location )) == null ? '' : __t) +'</p>\n\n';}return __p};
+  this["JST"]["app/scripts/templates/application.ejs"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<div id="startup-logo">\n    <img  src="images/itagenten-logo.png" />\n</div>\n\n';}return __p};
 
   this["JST"]["app/scripts/templates/detail.ejs"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<!-- <div class="detail"> </div> -->\n\n';}return __p};
 
